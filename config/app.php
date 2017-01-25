e13c6a844584f764645597d607ae56130397682d
James Makau <jacjimus@gmail.com>
2017-01-25 21:25:08 +0300

    Update on Wed 25th 8Am

--- config/app.php
+++ config/app.php
@@ -162,10 +162,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-       
 
         /*
          * Package Service Providers...
@@ -229,9 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-	'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
