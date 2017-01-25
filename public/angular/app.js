e465813d9b3f87a4f5a08bbe1912e6dc8818d6b3
James Makau <jacjimus@gmail.com>
2017-01-25 21:25:01 +0300

    Update on Wed 25th 8Am

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', [] , function($interpolateProvider) {
         $interpolateProvider.startSymbol('<%');
         $interpolateProvider.endSymbol('%>');
-         
+       //  $interpolateProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
     })
         .constant('API_URL', 'http://localhost:5310/blog/public/');
